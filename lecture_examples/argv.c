c3834074c490c215d12be20b07eda1fd75496868
Michael Rohs <michael.rohs@hci.uni-hannover.de>
2020-09-21 09:58:25 +0200

    variants

--- lecture_examples/argv.c
+++ lecture_examples/argv.c
@@ -1,13 +1,13 @@
 /*
 Compile: make argv
 Run: ./argv
-make argv && ./argv
+make argv && ./argv 1 2 3
 */
 
 #include "base.h"
-#include "string.h"
 
-// int main(int argc, char *argv[]) {
+// int main(int argc, char** argv) {
+// int main(int argc, char * argv[]) {
 int main(int argc, String argv[]) {
     for (int i = 0; i <= argc; i++) {
         printsln(argv[i]);
